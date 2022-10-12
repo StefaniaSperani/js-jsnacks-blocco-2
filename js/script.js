4d3cc3b133a12743412afb5ded50a588f53ac281
StefaniaSperani <thedarkcry@hotmail.it>
2022-10-12 15:42:49 +0200

    push

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
 // creo le variabili che mi servono all'interno del js
-const numeroUtente = parseInt(document.getElementById('numero').value);
+
 const btn = document.querySelector('button');
 const result = document.getElementById('risultato');
 
@@ -13,16 +13,19 @@ const result = document.getElementById('risultato');
 
 // creo una funzione che attaccherò al btn
 function check(){
-    // console.log('sono dentro la funzione')
+    console.log('sono dentro la funzione')
 
+    const numeroUtente = parseInt(document.getElementById('numero').value);
     // se il numero è pari, lo stampiamo in pagina
-    if(numeroUtente %2 === 0){
-        console.log(numeroUtente)
+    if(numeroUtente % 2 == 0){
+        console.log('numeroUtente')
 
         result.innerHTML = `${numeroUtente} è pari!`
+
     // altrimenti, se il numero è dispari stampiamo il suo valore MA +1
     }else{
-
+        numeroUtente = numeroUtente += 1 ;
+        result.innerHTML = `${numeroUtente}. Ho aggiunto +1 al tuo numero perchè era dispari!`
     }
 }
 btn.addEventListener('click', check);
