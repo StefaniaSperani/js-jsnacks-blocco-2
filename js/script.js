4dc1a42ca8b4807db267656dca8765a80f09accf
StefaniaSperani <thedarkcry@hotmail.it>
2022-10-12 15:50:19 +0200

    snack1 end

--- js/script.js
+++ js/script.js
@@ -13,18 +13,18 @@ const result = document.getElementById('risultato');
 
 // creo una funzione che attaccherò al btn
 function check(){
-    console.log('sono dentro la funzione')
+    // console.log('sono dentro la funzione')
 
-    const numeroUtente = parseInt(document.getElementById('numero').value);
+    let numeroUtente = parseInt(document.getElementById('numero').value);
     // se il numero è pari, lo stampiamo in pagina
     if(numeroUtente % 2 == 0){
-        console.log('numeroUtente')
+        // console.log('numeroUtente')
 
         result.innerHTML = `${numeroUtente} è pari!`
 
     // altrimenti, se il numero è dispari stampiamo il suo valore MA +1
     }else{
-        numeroUtente = numeroUtente += 1 ;
+        numeroUtente = numeroUtente + 1 ;
         result.innerHTML = `${numeroUtente}. Ho aggiunto +1 al tuo numero perchè era dispari!`
     }
 }
